be0f758205c3f7d934f6663454522fa27a4203cc
Sumit Bhade <imsumitbhade@gmail.com>
2025-01-18 03:46:58 +0530

    Validating user input upon lost focus (blur)

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -6,8 +6,12 @@ export default function Login() {
     password: "",
   });
 
-  const emailIsInvalid =
-    enteredValues.email !== "" && !enteredValues.email.includes("@");
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false,
+  });
+
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes("@");
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -23,6 +27,13 @@ export default function Login() {
     });
   }
 
+  function handleInputBlur(identifier) {
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: true,
+    }));
+  }
+
   return (
     <form onSubmit={handleSubmit}>
       <h2>Login</h2>
@@ -34,6 +45,7 @@ export default function Login() {
             id="email"
             type="email"
             name="email"
+            onBlur={() => handleInputBlur("email")}
             onChange={(event) => {
               handleInputChange("email", event.target.value);
             }}
@@ -51,6 +63,7 @@ export default function Login() {
             id="password"
             type="password"
             name="password"
+            onBlur={() => handleInputBlur("password")}
             onChange={(event) => {
               handleInputChange("password", event.target.value);
             }}
