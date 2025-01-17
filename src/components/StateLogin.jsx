e3242412602e7aa8cd0069ebb74eaca187ad8dff
Sumit Bhade <imsumitbhade@gmail.com>
2025-01-18 03:49:51 +0530

    Potential solution to problems in validation

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -25,6 +25,13 @@ export default function Login() {
         [identifier]: value,
       };
     });
+
+    //Solution
+
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: false,
+    }));
   }
 
   function handleInputBlur(identifier) {
